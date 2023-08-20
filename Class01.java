2c4ed179d5092216c609bd5b651ca066a40518f7
Abdullah <abdullahcesur0707@gmail.com>
2023-08-20 22:11:42 +0200

    v1

---
+++ Class01.java
@@ -0,0 +1,35 @@
+public class Class01 {
+    public static void main(String[] args) {
+
+        //Versiyon 1 kodlari
+        System.out.println("Versiyon1");
+
+        //Versiyon 2 kodlari
+        System.out.println("Versiyon2");
+
+        //Versiyon 3 Kodlari
+
+        // Versiyon 4
+        System.out.println("Versiyon");
+
+
+
+
+        
+
+        //versiyon 4 kodlari (hatali kodlar,unutulan bazi ozellikler olsun)
+        System.out.println("Hatali Kodlar");
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
